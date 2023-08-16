bd13ba0ded3ca48db9f9d44b1060dbb6102bb28b
Bivek10 <bibekkarki662@gmail.com>
2023-08-16 17:00:30 +0545

    feat: model for blood glucose

---
+++ lib/src/model/blood_glucose.dart
@@ -0,0 +1,8 @@
+class BloodGlucose {
+  final double value;
+  final String unit;
+  final DateTime dateFrom;
+  final DateTime dateTo;
+
+  BloodGlucose(this.value, this.unit, this.dateFrom, this.dateTo);
+}
