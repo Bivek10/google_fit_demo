fc9fa0efa1889f110588e03847db5511635a2a1a
Bivek10 <bibekkarki662@gmail.com>
2023-08-16 17:00:52 +0545

    FEAT: model class for foot steps

---
+++ lib/src/model/foot_setps.dart
@@ -0,0 +1,8 @@
+class FootSteps {
+  final double value;
+  final String unit;
+  final DateTime dateFrom;
+  final DateTime dateTo;
+
+  FootSteps(this.value, this.unit, this.dateFrom, this.dateTo);
+}
